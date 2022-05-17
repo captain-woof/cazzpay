1bdf1fa7eba96281df05634938df7c50d0c07e33
Rajarshi38 <rajarshibiswas38@gmail.com>
2022-05-18 01:17:40 +0530

    Fixed Normal things

--- client/lib/paypal_api.ts
+++ client/lib/paypal_api.ts
@@ -5,29 +5,33 @@ const base = "https://api-m.sandbox.paypal.com";
 /** CreateOrder,CaptureOrder & RefundOrder for payment  */
 //library function for creating an order
 export async function createOrder(amountPrice: string): Promise<string> {
-  const accessToken: string = await generateAccessToken();
-  const url: string = `${base}/v2/checkout/orders`;
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${accessToken}`,
-    },
-    body: JSON.stringify({
-      intent: "CAPTURE",
-      purchase_units: [
-        {
-          amount: {
-            currency_code: "USD",
-            value: amountPrice,
+  try {
+    const accessToken: string = await generateAccessToken();
+    const url: string = `${base}/v2/checkout/orders`;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        intent: "CAPTURE",
+        purchase_units: [
+          {
+            amount: {
+              currency_code: "USD",
+              value: amountPrice,
+            },
           },
-        },
-      ],
-    }),
-  });
+        ],
+      }),
+    });
 
-  const data = await response.json();
-  return data;
+    const data = await response.json();
+    return data;
+  } catch (error: any) {
+    return error.message;
+  }
 }
 //library function to capture the payment of the order based on orderId
 export async function capturePayment(orderId: string) {
@@ -67,32 +71,40 @@ export async function capturePayment(orderId: string) {
 //incase of any error happens while minting the token or the token could not generated
 //we have to refund the payment
 export async function refundPayment(paymentId: string) {
-  const access_token: string = await generateAccessToken();
-  const url = `${base}/v2/payments/captures/${paymentId}/refund`;
-  const response = await fetch(url, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${access_token}`,
-    },
-  });
-  const data = await response.json();
-  return data;
+  try {
+    const access_token: string = await generateAccessToken();
+    const url = `${base}/v2/payments/captures/${paymentId}/refund`;
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+    const data = await response.json();
+    return data;
+  } catch (error: any) {
+    return error.message;
+  }
 }
 
 //helper function for generating access token via paypal client id and secret
 export async function generateAccessToken(): Promise<string> {
-  const auth: string = Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString(
-    "base64"
-  );
-  const response = await fetch(`${base}/v1/oauth2/token`, {
-    method: "post",
-    body: "grant_type=client_credentials",
-    headers: {
-      Authorization: `Basic ${auth}`,
-    },
-  });
-  const data = await response.json();
-  return data.access_token;
+  try {
+    const auth: string = Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString(
+      "base64"
+    );
+    const response = await fetch(`${base}/v1/oauth2/token`, {
+      method: "post",
+      body: "grant_type=client_credentials",
+      headers: {
+        Authorization: `Basic ${auth}`,
+      },
+    });
+    const data = await response.json();
+    return data.access_token;
+  } catch (error: any) {
+    return error.message;
+  }
 }
 /** Till here is for order payment */
 
@@ -102,60 +114,112 @@ export async function generateAccessToken(): Promise<string> {
 export async function generateAccessTokenForCustomer(
   authorization: string
 ): Promise<PayPalToken> {
-  const auth: string = Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString(
-    "base64"
-  );
-  const url = `${base}/v1/oauth2/token`;
-  const response = await fetch(url, {
-    method: "post",
-    body: `grant_type=authorization_code&code=${authorization}`,
-    headers: {
-      Authorization: `Basic ${auth}`,
-    },
-  });
-  const data = await response.json();
-  return {
-    accessToken: data.access_token,
-    refreshToken: data.refresh_token,
-    expiresIn: data.expires_in,
-  };
+  try {
+    const auth: string = Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString(
+      "base64"
+    );
+    const url = `${base}/v1/oauth2/token`;
+    const response = await fetch(url, {
+      method: "post",
+      body: `grant_type=authorization_code&code=${authorization}`,
+      headers: {
+        Authorization: `Basic ${auth}`,
+      },
+    });
+    const data = await response.json();
+    return {
+      accessToken: data.access_token,
+      refreshToken: data.refresh_token,
+      expiresIn: data.expires_in,
+    };
+  } catch (error: any) {
+    return error.message;
+  }
 }
 
 //fetching the customer data via access_token
 export async function getCustomerData(accessToken: string) {
-  const url = `${base}/v1/identity/oauth2/userinfo?schema=paypalv1.1`;
-  const response = await fetch(url, {
-    method: "get",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
-  const data = await response.json();
-  const customerData: PaypalProfileProps = {
-    name: data.name,
-    paypalId: data.payer_id,
-    email: data.emails[0].value,
-  };
-  return customerData;
+  try {
+    const url = `${base}/v1/identity/oauth2/userinfo?schema=paypalv1.1`;
+    const response = await fetch(url, {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    const data = await response.json();
+    const customerData: PaypalProfileProps = {
+      name: data.name,
+      paypalId: data.payer_id,
+      email: data.emails[0].value,
+    };
+    return customerData;
+  } catch (error: any) {
+    return error.message;
+  }
 }
 
 //api function for getting access_token via refresh_token if access_token is expired
 export async function refreshAccessToken(
   refreshToken: string
 ): Promise<string> {
-  const auth: string = Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString(
-    "base64"
-  );
-  const url = `${base}/v1/oauth2/token`;
-  const response = await fetch(url, {
-    method: "post",
-    body: `grant_type=refresh_token&refresh_token=${refreshToken}`,
-    headers: {
-      Authorization: `Basic ${auth}`,
-    },
-  });
+  try {
+    const auth: string = Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString(
+      "base64"
+    );
+    const url = `${base}/v1/oauth2/token`;
+    const response = await fetch(url, {
+      method: "post",
+      body: `grant_type=refresh_token&refresh_token=${refreshToken}`,
+      headers: {
+        Authorization: `Basic ${auth}`,
+      },
+    });
 
-  const data = await response.json();
-  return data.access_token;
+    const data = await response.json();
+    return data.access_token;
+  } catch (error: any) {
+    return error.message;
+  }
+}
+
+/** Sending money from the merchant account is complex we need to use payout rest api here
+ *  we need to create payout batch
+ *
+ */
+
+export async function payoutSeller(
+  amountToPay: string | number,
+  sellerId: string
+) {
+  try {
+    const access_token: string = await generateAccessToken();
+    const url = `${base}/v1/payments/payouts`;
+
+    const response = await fetch(url, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${access_token}`,
+      },
+      body: JSON.stringify({
+        sender_batch_header: {},
+        items: [
+          {
+            recipient_type: "PAYPAL_ID",
+            amount: {
+              currency: "USD",
+              amount: amountToPay,
+            },
+            reciever: sellerId,
+          },
+        ],
+      }),
+    });
+    const data = await response.json();
+    return data.payout_batch_id;
+  } catch (error: any) {
+    return error.message;
+  }
 }
