2a0c88c4c8073cefeb5f157f05e3ad6d7babace5
captain-woof <captainwoof.official@gmail.com>
2022-05-17 17:24:12 +0530

    README and HardhatConfig updated

--- contracts/README.md
+++ contracts/README.md
@@ -17,3 +17,17 @@ yarn NAME_OF_SCRIPT
 ```bash
 npx hardhat run PATH/TO/SCRIPT --network NETWORK
 ```
+
+## Testing
+
+```bash
+npx hardhat test ./test/NAME_OF_FILE
+```
+
+# Environment variables
+
+```env
+INFURA_PROJECT_ID # From infura project
+PRIVATE_KEY # To use for deploying contracts
+ETHERSCAN_API_KEY # To verify contracts
+```
--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -3,20 +3,18 @@ import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
 
 dotenv.config({ path: "./.env" });
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+/*task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
     console.log(account.address);
   }
-});
+});*/
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -34,21 +32,15 @@ const config: HardhatUserConfig = {
     }))
   },
   networks: {
-    polygon: {
-      url: "https://polygon-rpc.com/",
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    polygonMumbai: {
-      url: "https://rpc-mumbai.matic.today/",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
+    }
   },
   etherscan: {
     apiKey: {
-      polygon: process.env.POLYGONSCAN_API_KEY as string,
-      polygonMumbai: process.env.POLYGONSCAN_API_KEY as string,
+      rinkeby: process.env.ETHERSCAN_API_KEY as string,
     },
   },
 };
