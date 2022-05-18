d27e36b6587c524e0cd6f7675695b8975138ac13
captain-woof <captainwoof.official@gmail.com>
2022-05-18 12:35:11 +0530

    Chakra installed; theme set; fonts set

---
+++ client/theme/theme.ts
@@ -0,0 +1,21 @@
+import { Theme, extendTheme, withDefaultColorScheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+
+const theme = extendTheme({
+    fonts: {
+        body: "'Ubuntu', sans-serif",
+        heading: "'Koulen', cursive"
+    },
+    config: {
+        initialColorMode: "system"
+    },
+    styles: {
+        global: (props: any) => ({
+            body: {
+                backgroundColor: mode("gray.50", "gray.700")(props)
+            }
+        })
+    }
+}, withDefaultColorScheme({ colorScheme: "blue" })) as Theme;
+
+export default theme;
