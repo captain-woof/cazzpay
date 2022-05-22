d996ca4090fedf5db3c3b863bc29999e7dbd7182
captain-woof <captainwoof.official@gmail.com>
2022-05-22 23:55:48 +0530

    Fonts self-hosted

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -1,16 +1,11 @@
 import { ColorModeScript } from '@chakra-ui/react';
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import NextDocument, { Html, Main, NextScript } from 'next/document';
 import theme from '../theme';
 
 export default class Document extends NextDocument {
     render() {
         return (
             <Html lang='en'>
-                <Head>
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                    <link href="https://fonts.googleapis.com/css2?family=Koulen&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet" />
-                </Head>
                 <body>
                     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
                     <Main />
